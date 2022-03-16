4af5ffb028970d08ab6118ff4b3c6a2f65a3f0cf
денис шумаков <vip.shumakov35@mail.ru>
2022-03-16 13:17:59 +0300

    task11-complete

--- task11.py
+++ task11.py
@@ -6,10 +6,8 @@ from PIL import Image
 
 def MakeGraphGreatAgain(V, E):
     graph = nx.Graph()
-    V = list(map(int, V.split()))
-    E = list(map(int, E.split()))
-    # print(V)
-    # print(E)
+    V = list(map(int, V.replace('{', '').replace('}', '').replace(',', ' ').split()))
+    E = list(map(int, E.replace('{', '').replace('}', '').replace('(', '').replace(')', '').replace(',', ' ').split()))
     res = []
     temp = 0
     tmp = []
@@ -25,32 +23,78 @@ def MakeGraphGreatAgain(V, E):
     nx.draw_circular(graph, node_color='red', node_size=1000, with_labels=True)
     plt.savefig("Graf.png")
     plt.show()
-    # MakePicture()
-    print(dfs(res))
+    resE = []
+    for i in range(0, len(E), 2):
+        resE.append((int(E[i]), int(E[i + 1])))
+    # print(resE)
+    dfs(V, resE)
     return 0
 
-# def MakePicture():
-#     init_colorit()
-#     depth = 100
-#     image = Image.open("Graf.png")
-#     image = image.resize((depth, depth))
-#     for y in range(image.size[1]):
-#         for x in range(image.size[0]):
-#             print(background(" ", image.getpixel((x, y))), end='')
-#         print()
 
+def dfs(V, E):
+    res = E[0]
+    for i in range(1, len(E)):
+        tmp = []
+        poglotitels = []
+        for first_elem in res:
+            for second_elem in E[i]:
+                if str(first_elem) in str(second_elem):
+                    tmp.append(second_elem)
+                    poglotitels.append(second_elem)
+                elif str(second_elem) in str(first_elem):
+                    tmp.append(first_elem)
+                    poglotitels.append(first_elem)
+                else:
+                    new_elem = ''.join(sorted(list(set(char for char in str(first_elem) + str(second_elem)))))
+                    tmp.append(int(new_elem))
+        res = list(set(tmp))
+        poglotitels = list(set(poglotitels))
+        for pop in poglotitels:
+            res = test_pogloshenie(res, pop)
 
-def dfs(res):
-    print("Все работатет")
+    final_stage = []
+    for i in res:
+        final_stage.append(list(map(int, [char for char in str(i)])))
+
+    for i in range(len(final_stage)):
+        final_stage[i] = tuple(set(V) - set(final_stage[i]))
+
+    final_stage_lengths = dict()
+
+    for i in final_stage:
+        if len(i) in final_stage_lengths:
+            final_stage_lengths[len(i)].append(i)
+        else:
+            final_stage_lengths[len(i)] = [i]
+    print(final_stage, 'Максимальные внутренние устойчивые множества')
+    print(final_stage_lengths[max(final_stage_lengths.keys())],'Наибольшие внутренне устойчивые множества')
     return 0
 
-# V = input()
-V = "{1,2,3,4,5,6,7}"
-V = V.replace('{','' ).replace('}','').replace(',',' ')
-# E = input()
-E = "{(1,2),(1,3),(1,5),(1,6),(2,3),(3,4),(3,6),(4,5),(4,7),(5, 6),(6,7)}"
-E = E.replace('{','' ).replace('}','').replace('(','' ).replace(')','').replace(',',' ')
+
+def pogloshenie(poglotitel, res):
+    pogl = [i for i in poglotitel]
+    r = [i for i in res]
+    flag = True
+    for p in pogl:
+        if p not in r:
+            flag = False
+    return flag
+
+
+def test_pogloshenie(res, poglotitel):
+    final_res = []
+    for r in res:
+        if poglotitel == r or not pogloshenie(str(poglotitel), str(r)):
+            final_res.append(r)
+    return list(set(final_res))
+
+print("Введите вершины: ")
+V = input()
+# V = "{1,2,3,4,5,6,7}"
+print("Введите ребра: ")
+E = input()
+# E = "(1,2) (1,3) (1,5) (1,6) (2,3) (3,4) (3,6) (4,5) (4,7) (5,6) (6,7)"
 MakeGraphGreatAgain(V, E)
 
-# {1,2,3,4,5,6,7}
-# {(1,2),(1,3),(1,4),(2,3)
+# (V={1,2,3,4,5,6,7}, E={(1,2),(1,4),(1,6),(1,7),(2,4),(2,6),(3,4),(3,5),(3,6),(3,7),(4,5),(5,6),(6,7)})
+# (V={1,2,3,4,5,6,7}, E={(1,2),(1,3),(1,4),(1,6),(2,3),(2,6),(3,4),(3,6),(3,7),(4,5),(4,7),(5,6),((6,7)}).
